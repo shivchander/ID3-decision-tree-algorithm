cca8fe3560b45f47e17ab9fe1d938a763216ab4c
shiv <sudalasr@mail.uc.edu>
2020-11-12 18:27:48 -0500

    Working DT

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Decision Tree Implementation from scratch using Iris Dataset
 '''
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import balanced_accuracy_score, fbeta_score
+from sklearn.metrics import balanced_accuracy_score, fbeta_score, confusion_matrix
 from ID3 import ID3DecisionTree
 from utils import *
 
@@ -19,6 +19,7 @@ def main():
     f1_beta_half_dict = {}
     f1_beta_1_dict = {}
     f1_beta_2_dict = {}
+    roc_dict = {}
     for bins in [5, 10, 15, 20]:
         accs = []
         f1_beta_half = []
@@ -36,15 +37,28 @@ def main():
             f1_beta_1.append(fbeta_score(true, pred, beta=1))
             f1_beta_2.append(fbeta_score(true, pred, beta=2))
         acc_dict[bins] = accs
+        roc_dict[bins] = get_fpr_tpr(true, pred)
         f1_beta_half_dict[bins] = f1_beta_half
         f1_beta_1_dict[bins] = f1_beta_1
         f1_beta_2_dict[bins] = f1_beta_2
 
     print(print_accuracies(acc_dict))
     plot_accuracies(acc_dict, 'Decision Tree Accuracy')
-    plot_f1(f1_beta_half_dict, 'Decision Tree F1 Score (Beta = 0.5)')
-    plot_f1(f1_beta_1_dict, 'Decision Tree F1 Score (Beta = 1)')
-    plot_f1(f1_beta_2_dict, 'Decision Tree F1 Score (Beta = 2)')
+    # plot_f1(f1_beta_half_dict, 'Decision Tree F1 Score (Beta = 0.5)')
+    # plot_f1(f1_beta_1_dict, 'Decision Tree F1 Score (Beta = 1)')
+    # plot_f1(f1_beta_2_dict, 'Decision Tree F1 Score (Beta = 2)')
+    plot_roc(roc_dict, 'Decision Tree ROC Curve')
+
+
+def test():
+    x, y = load_dataset()
+    dt = ID3DecisionTree(nbins=5)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+    tree = dt.fit(X_train, y_train)
+    y_hat = dt.predict(X_test)
+    true = np.where(y_test == 'setosa', 0, 1)
+    pred = np.where(y_hat == 'setosa', 0, 1)
+    print(get_tpr_fpr(true, pred))
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ Decision Tree Implementation from scratch using Iris Dataset
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
+from sklearn.metrics import confusion_matrix
 
 
 def load_dataset():
@@ -51,3 +52,22 @@ def plot_f1(f1_dict, title):
     plt.title(title)
     plt.savefig('figs/' + title + '.png')
     plt.clf()
+
+
+def get_fpr_tpr(true, pred):
+    tn, fp, fn, tp = confusion_matrix(true, pred).ravel()
+    sensitivity = tp / (tp + fn)
+    specificity = tn / (tn + fp)
+
+    return 1-specificity, sensitivity
+
+
+def plot_roc(roc_dict, title):
+    plt.plot(np.arange(0, 1.1, 0.1), np.arange(0, 1.1, 0.1), 'r--', label='Random Guess')
+    for bin in roc_dict:
+        plt.plot(roc_dict[bin][0], roc_dict[bin][1], marker='o', label='bin'+str(bin))
+    plt.legend()
+    plt.xlabel('FPR (1-specificity)')
+    plt.ylabel('TPR (sensitivity)')
+    plt.title(title)
+    plt.savefig('figs/' + title + '.png')
